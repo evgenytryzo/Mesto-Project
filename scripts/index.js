d6ec171cb232d1fd0468a408eaf70cd29b28b825
tryzo <g0022@inbox.ru>
2023-03-25 01:42:23 +0300

    mesto_25-03-2023_01-42

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,22 @@ const moreInfoPopupClose = moreInfoPopup.querySelector('.popup__close');
 const nameInput = moreInfoPopup.querySelector('.popup__input_type_name');
 const moreInfoPopupForm = moreInfoPopup.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
-const detailInput = moreInfoPopup.querySelector('.popup__input_type_detail')
-const profileDetail = document.querySelector('.profile__details')
+const detailInput = moreInfoPopup.querySelector('.popup__input_type_detail');
+const profileDetail = document.querySelector('.profile__details');
 
 editButtonLink.addEventListener('click', () => {
-    moreInfoPopup.classList.add('popup_opened')
+    moreInfoPopup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     detailInput.value = profileDetail.textContent;
 })
 
 moreInfoPopupClose.addEventListener('click', () => {
-    moreInfoPopup.classList.remove('popup_opened')
+    moreInfoPopup.classList.remove('popup_opened');
 })
 
 moreInfoPopupForm.addEventListener('submit', (event) => {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileDetail.textContent = detailInput.value;
-    moreInfoPopup.classList.remove('popup_opened')
+    moreInfoPopup.classList.remove('popup_opened');
 })
