d3c35a1ad4b28cd37f3880f24787dd538ac54cfc
evgenytryzo <g0022@inbox.ru>
2023-04-30 16:32:44 +0300

    mesto_30-04-2023_16-32

--- scripts/validate.js
+++ scripts/validate.js
@@ -35,7 +35,6 @@ const enableButton = (button) => {
 
 const toggleButtonValidity = (formEdit) => {
     const submitButton = formEdit.querySelector('.popup__submit-btn');
-
     if (formEdit.checkValidity()) {
         enableButton(submitButton)
     } else {
@@ -50,36 +49,48 @@ const setSubmitListener = (formEdit) => {
     });
 }
 
-function enableValidation() {
-    const form = document.querySelector('.popup__form_edit');
-    setSubmitListener(form);
+const forms = () => {
+    const forms = document.querySelectorAll('.popup__form')
+    const formsArr = Array.from(forms)
+
+    formsArr.forEach((input) => {
+
+        enableValidation(input)
+        toggleButtonValidity(input)
+    })
+}
+
+
+function enableValidation(form) {
 
     const inputsEdit = document.querySelectorAll('.popup__input');
     const inputsArrayEdit = Array.from(inputsEdit);
 
-    const checkValidityButton = document.querySelector('.profile__edit-button');
+    const checkValidityButtonEdd = document.querySelector('.profile__edit-button');
+    const checkValidityButtonAdd = document.querySelector('.profile__add-button')
 
     inputsArrayEdit.forEach((input) => {
         input.addEventListener('input', () => {
-            checkInputValidity(input)
-            toggleButtonValidity(form)
-            checkValidityButton.addEventListener('click', () => {
-                toggleButtonValidity(form)
-                checkInputValidity(input)
+            checkInputValidity(input);
+            toggleButtonValidity(form);
+
+            checkValidityButtonEdd.addEventListener('click', () => {
+                toggleButtonValidity(form);
+                checkInputValidity(input);
             })
+
+            checkValidityButtonAdd.addEventListener('click', () => {
+                const errElement = document.querySelector(`#err-${input.id}`)
+                setInputInvalidState(input, errElement)
+            })
+
         })
+
     })
 
-
-
+    setSubmitListener(form)
 
 }
 
-enableValidation({
-    formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__submit-btn",
-    inactiveButtonClass: "popup__submit-btn_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible",
-});
+forms()
+
