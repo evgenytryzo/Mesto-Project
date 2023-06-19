1c574ba69db427b433bd742b32e669a3faaeedac
evgenytryzo <g0022@inbox.ru>
2023-06-20 00:28:41 +0300

    Add Popup class, add inherited PopupWithImage class

---
+++ src/scripts/Popup.js
@@ -0,0 +1,27 @@
+export default class Popup {
+  constructor (selector) {
+    this._selector = selector
+    this._closeButton = this._selector.querySelector(".popup__close")
+  }
+
+  open () {
+    this._selector.classList.add("popup_opened")
+    document.addEventListener("keydown", this._handleEscClose)
+  }
+
+  close () {
+    document.removeEventListener("keydown", this._handleEscClose)
+    this._selector.classList.remove("popup_opened")
+  }
+
+  _handleEscClose (event) {
+    if ( event.key === "Escape" ) this.close()
+  }
+
+  setEventListeners () {
+    this._closeButton.addEventListener("click", () => this.close())
+    this._selector.addEventListener("mousedown", (event) => {
+      if ( event.target === event.currentTarget ) this.close()
+    })
+  }
+}
---
+++ src/scripts/PopupWithImage.js
@@ -0,0 +1,15 @@
+import Popup from "./Popup"
+
+export default class PopupWithImage extends Popup {
+  constructor (selector) {
+    super(selector)
+    this._popupImage = this._selector.querySelector(".popup__image")
+    this._popupImageName = this._selector.querySelector(".popup__image-name")
+  }
+  open (name, link) {
+    this._popupImageContainer.src = link
+    this._popupImageContainer.alt = name
+    this._popupImageName.textContent = name
+    super.open()
+  }
+}
