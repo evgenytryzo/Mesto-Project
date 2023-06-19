bc4fbd48f4e89d33ce9f00f264ac82fbd21c9b1d
evgenytryzo <g0022@inbox.ru>
2023-06-19 22:15:34 +0300

    Completed Webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,22 +3,22 @@ import { initialCards, config } from "./constants.js"
 import Card from "./Card.js"
 import FormValidator from "./FormValidator.js"
 
-const editButtonLink = document.querySelector('.profile__edit-button-link')
-const moreInfoPopup = document.querySelector('.popup_type_edit')
-const moreInfoPopupForm = moreInfoPopup.querySelector('.popup__form')
-const profileName = document.querySelector('.profile__name')
-const detailInput = moreInfoPopup.querySelector('.popup__input_type_detail')
-const buttonAdd = document.querySelector('.profile__add-button')
-const popupAdd = document.querySelector('.popup_type_add')
-const popupAddForm = document.querySelector('.popup__form_add')
-const popupEddForm = document.querySelector('.popup__form_edit')
+const editButtonLink = document.querySelector(".profile__edit-button-link")
+const moreInfoPopup = document.querySelector(".popup_type_edit")
+const moreInfoPopupForm = moreInfoPopup.querySelector(".popup__form")
+const profileName = document.querySelector(".profile__name")
+const detailInput = moreInfoPopup.querySelector(".popup__input_type_detail")
+const buttonAdd = document.querySelector(".profile__add-button")
+const popupAdd = document.querySelector(".popup_type_add")
+const popupAddForm = document.querySelector(".popup__form_add")
+const popupEddForm = document.querySelector(".popup__form_edit")
 const popups = document.querySelectorAll(".popup")
-const elements = document.querySelector('.elements')
-const elementTemplate = document.querySelector('.elements-template')
-const nameInputAdd = popupAddForm.querySelector('.popup__input_type_name')
-const linkInputAdd = popupAddForm.querySelector('.popup__input_type_link')
-const nameInputEdd = popupEddForm.querySelector('.popup__input_type_name')
-const profileDetail = document.querySelector('.profile__details')
+const elements = document.querySelector(".elements")
+const elementTemplate = document.querySelector(".elements-template")
+const nameInputAdd = popupAddForm.querySelector(".popup__input_type_name")
+const linkInputAdd = popupAddForm.querySelector(".popup__input_type_link")
+const nameInputEdd = popupEddForm.querySelector(".popup__input_type_name")
+const profileDetail = document.querySelector(".profile__details")
 
 const createNewCard = (element, openPopup, template) => {
   const card = new Card(element, openPopup, template)
@@ -32,19 +32,19 @@ const profileFormValidator = new FormValidator(config, popupEddForm)
 profileFormValidator.enableValidation()
 
 const openPopup = (popupElement) => {
-  popupElement.classList.add('popup_opened')
-  document.addEventListener('keydown', closePopupEsc)
+  popupElement.classList.add("popup_opened")
+  document.addEventListener("keydown", closePopupEsc)
 }
 
 const closePopupEsc = (event) => {
   if ( event.key === "Escape" ) {
-    const popup = document.querySelector('.popup_opened')
+    const popup = document.querySelector(".popup_opened")
     closePopup(popup)
   }
 }
 
 const closePopup = (popupElement) => {
-  popupElement.classList.remove('popup_opened')
+  popupElement.classList.remove("popup_opened")
   document.removeEventListener("keydown", closePopupEsc)
 }
 
@@ -70,14 +70,14 @@ initialCards.forEach(element => {
   renderElementAdd(createNewCard(element, openPopup, elementTemplate))
 })
 
-editButtonLink.addEventListener('click', () => {
+editButtonLink.addEventListener("click", () => {
   openPopup(moreInfoPopup)
   nameInputEdd.value = profileName.textContent
   detailInput.value = profileDetail.textContent
   profileFormValidator.resetValidation(popupEddForm)
 })
 
-buttonAdd.addEventListener('click', () => {
+buttonAdd.addEventListener("click", () => {
   popupAddForm.reset()
   openPopup(popupAdd)
   cardFormValidator.resetValidation(popupAddForm)
@@ -90,18 +90,18 @@ const popupAddSubmit = (event) => {
   const link = linkInputAdd.value
   const elementData = {
     name,
-    link,
+    link
   }
 
   renderElement(createNewCard(elementData, openPopup, elementTemplate))
   closePopup(popupAdd)
 }
 
-moreInfoPopupForm.addEventListener('submit', (event) => {
+moreInfoPopupForm.addEventListener("submit", (event) => {
   event.preventDefault()
   profileName.textContent = nameInputEdd.value
   profileDetail.textContent = detailInput.value
   closePopup(moreInfoPopup)
 })
 
-popupAddForm.addEventListener('submit', popupAddSubmit)
+popupAddForm.addEventListener("submit", popupAddSubmit)
